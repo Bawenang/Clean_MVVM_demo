470148d451aa2cec60a29e02cd85d9ae355527f5
Bawenang Rukmoko <bawenang.rukmoko@zyrous.com>
2021-11-28 20:49:52 +0700

    Implement View Model + Swinject

---
+++ cleanMVVM/cleanMVVM/Presentation/Swinject/SwinjectStoryboard.swift
@@ -0,0 +1,31 @@
+//
+//  SwinjectStoryboard.swift
+//  cleanMVVM
+//
+//  Created by Bawenang RPP on 28/11/21.
+//
+
+import Swinject
+import SwinjectStoryboard
+
+extension SwinjectStoryboard {
+    static func setup() {
+        Container.loggingFunction = nil
+        setupViewModels()
+        setupViewControllers()
+    }
+}
+
+extension SwinjectStoryboard {
+    class func setupViewControllers() {
+        defaultContainer.storyboardInitCompleted(MainViewController.self) { (r, c) in
+            c.viewModel = r.resolve(MainViewModelling.self)!
+        }
+    }
+    class func setupViewModels() {
+        defaultContainer.register(MainViewModelling.self) { _ in
+            let getAllUserUseCase = SwinjectDomain.container.resolve(GetAllUser.self)!
+            return MainViewModel(userListProvider: getAllUserUseCase.get)
+        }
+    }
+}
--- cleanMVVM/cleanMVVM/Presentation/ViewControllers/MainViewController.swift
+++ cleanMVVM/cleanMVVM/Presentation/ViewControllers/MainViewController.swift
@@ -6,15 +6,22 @@
 //
 
 import UIKit
+import RxSwift
 
 class MainViewController: UIViewController {
     @IBOutlet weak var usersTableView: UITableView!
     
+    var viewModel: MainViewModelling!
+    
+    private let disposeBag = DisposeBag()
     private let reuseCellId = "userTableViewCell"
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupTableView()
+        setupViewModel()
+        
+        viewModel.loadUsers.accept(())
     }
     
     private func setupTableView() {
@@ -24,8 +31,21 @@ class MainViewController: UIViewController {
             .disposed(by: disposeBag)
     }
 
+    private func setupViewModel() {
+        viewModel.userParams
+            .drive(usersTableView.rx.items) { [weak self] tableView, _, element in
+                guard let `self` = self,
+                      let dequeuedCell = tableView.dequeueReusableCell(withIdentifier: self.reuseCellId),
+                      let cell = dequeuedCell as? UserTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.setup(with: element)
+                return cell
+            }
+            .disposed(by: disposeBag)
         
     }
+}
 
 extension MainViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
