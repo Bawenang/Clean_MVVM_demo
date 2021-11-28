54b547bc42516fcda6b2e283d8d47fc8e557bffc
Bawenang Rukmoko <bawenang.rukmoko@zyrous.com>
2021-11-28 20:49:29 +0700

    Implement "UsersTableView"

--- cleanMVVM/cleanMVVM/Presentation/ViewControllers/MainViewController.swift
+++ cleanMVVM/cleanMVVM/Presentation/ViewControllers/MainViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    @IBOutlet weak var usersTableView: UITableView!
+    
+    private let reuseCellId = "userTableViewCell"
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        usersTableView.register(UINib(nibName: "UserTableViewCell", bundle: nil),
+                                forCellReuseIdentifier: reuseCellId)
+        usersTableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
     }
 
+        
+    }
 
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 }
-
